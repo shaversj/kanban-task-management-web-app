8d5d76541f240d5b0a91b14c1d410fe8324af2a4
Josh Shavers <shaversj@gmail.com>
2025-07-21 11:11:15 -0500

    style second grid

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -38,7 +38,30 @@ function App() {
           </div>
         </div>
 
-        <div className={"bg-dark-grey border-dark-grey col-start-2 col-end-3 row-start-1 row-end-1 border border-b-[#3e3f4e]"}>Stuff</div>
+        <div className={"bg-dark-grey border-dark-grey col-start-2 col-end-3 row-start-1 row-end-1 flex items-center border border-b-[#3e3f4e] px-6"}>
+          <h1 className={"text-[20px] font-bold text-black dark:text-white"}>Platform Launch</h1>
+
+          <div className={"ml-auto flex items-center gap-x-6"}>
+            <button
+              className={
+                "bg-main-purple flex h-[48px] w-[164px] items-center justify-center gap-x-2 rounded-full text-[15px] font-bold text-black dark:text-white"
+              }
+            >
+              <svg width="12" height="12" xmlns="http://www.w3.org/2000/svg">
+                <path fill="#FFF" d="M7.368 12V7.344H12V4.632H7.368V0H4.656v4.632H0v2.712h4.656V12z" />
+              </svg>
+              Add New Task
+            </button>
+
+            <svg width="5" height="20" xmlns="http://www.w3.org/2000/svg">
+              <g fill="#828FA3" fill-rule="evenodd">
+                <circle cx="2.308" cy="2.308" r="2.308" />
+                <circle cx="2.308" cy="10" r="2.308" />
+                <circle cx="2.308" cy="17.692" r="2.308" />
+              </g>
+            </svg>
+          </div>
+        </div>
 
         <div className={"col-start-2 row-start-2 bg-[#20212c]"}>More Stuff</div>
       </main>
